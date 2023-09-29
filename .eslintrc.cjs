518c0ce82d9ebc6249998114e1b2a30927427268
sn0wdr1am <sn0wdr1am@qq.com>
2023-09-29 20:46:59 +0800

    chore: eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution")
+
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "@vue/eslint-config-typescript"
+  ],
+  "rules": {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "off"
+    ],
+    "vue/no-v-html": "off"
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslint({
+      cache: true,
+      fix: true,
+      eslintPath: 'eslint',
+      include:['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.vue', '**/*.svelte'],
+      exclude:['**/node_modules/**'],
+      lintOnStart: true,
+      emitWarning: true,
+      emitError: true,
+      failOnWarning: true,
+      failOnError: true,
+      formatter: 'stylish'
+    })
+  ],
 })
