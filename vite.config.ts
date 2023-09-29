0ec56700cff7dd9f6710fa09a19ffa42d321859f
sn0wdr1am <sn0wdr1am@qq.com>
2023-09-29 21:16:12 +0800

    chore: stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
+import stylelint from 'vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,14 +11,33 @@ export default defineConfig({
       cache: true,
       fix: true,
       eslintPath: 'eslint',
-      include:['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.vue', '**/*.svelte'],
-      exclude:['**/node_modules/**'],
+      include: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.vue', '**/*.svelte'],
+      exclude: ['**/node_modules/**'],
       lintOnStart: true,
       emitWarning: true,
       emitError: true,
       failOnWarning: true,
       failOnError: true,
       formatter: 'stylish'
+    }),
+    stylelint({
+      test: false,
+      dev: true,
+      build: false,
+      cache: true,
+      cacheLocation: '.stylelintcache',
+      include: ['src/**/*.{css,scss,sass,less,styl,vue,svelte}'],
+      exclude: ['node_modules', 'virtual:'],
+      stylelintPath: 'stylelint',
+      formatter: 'string',
+      lintInWorker: true,
+      lintOnStart: true,
+      lintDirtyOnly: true,
+      chokidar: false,
+      emitWarning: true,
+      emitWarningAsError: false,
+      emitError: true,
+      emitErrorAsWarning: false
     })
   ],
 })
