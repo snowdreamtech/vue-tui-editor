82a5a14b0b2399d04d7292bdab9cabc6a190dc4d
sn0wdr1am <sn0wdr1am@qq.com>
2023-09-30 21:52:43 +0800

    feat: example

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,14 +1,94 @@
 <template>
-  <Editor :initialValue="content" style="width:100%;" height="800px" />
+  <h1>Basic</h1>
+  <Editor :initialValue="content" />
+
+  <h1>With All Plugins</h1>
+  <Editor :initialValue="fullcontent" :options="editorOptions" />
 </template>
 
 <script setup lang="ts">
-import '@toast-ui/editor/dist/toastui-editor.css';
-
-import { Editor } from '@snowdreamtech/vue-tui-editor';
 import { ref } from 'vue';
 
+import { Editor } from '@snowdreamtech/vue-tui-editor';
+
+import '@toast-ui/editor/dist/toastui-editor.css'
+
+// language
+import '@toast-ui/editor/dist/i18n/zh-CN'
+// import '@toast-ui/editor/dist/i18n/zh-TW'
+// import '@toast-ui/editor/dist/i18n/en-US'
+// import '@toast-ui/editor/dist/i18n/ja-JP'
+// import '@toast-ui/editor/dist/i18n/es-ES'
+
+import '@toast-ui/chart/dist/toastui-chart.css'
+import chart from '@toast-ui/editor-plugin-chart'
+
+import uml from '@toast-ui/editor-plugin-uml'
+
+import '@toast-ui/editor-plugin-table-merged-cell/dist/toastui-editor-plugin-table-merged-cell.css'
+import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell'
+
+import 'tui-color-picker/dist/tui-color-picker.css'
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css'
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax'
+
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css'
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js'
+
+
+const chartOptions = {
+  minWidth: 100,
+  maxWidth: 600,
+  minHeight: 100,
+  maxHeight: 300
+}
+
+const plugins: any = [[chart, chartOptions], uml, tableMergedCell, colorSyntax, codeSyntaxHighlight]
+
+
 const content = ref(
+`
+![image](https://uicdn.toast.com/toastui/img/tui-editor-bi.png)
+
+# Awesome Editor!
+
+It has been _released as opensource in 2018_ and has ~~continually~~ evolved to **receive 10k GitHub ⭐️ Stars**.
+
+## Create Instance
+
+You can create an instance with the following code and use \`getHtml()\` and \`getMarkdown()\` of the [Editor](https://github.com/nhn/tui.editor).
+
+\`\`\`js
+const editor = new Editor(options);
+\`\`\`
+
+> See the table below for default options
+> > More API information can be found in the document
+
+| name | type | description |
+| --- | --- | --- |
+| el | \`HTMLElement\` | container element |
+
+## Features
+
+* CommonMark + GFM Specifications
+   * Live Preview
+   * Scroll Sync
+   * Auto Indent
+   * Syntax Highlight
+        1. Markdown
+        2. Preview
+
+## Support Wrappers
+
+> * Wrappers
+>    1. [x] React
+>    2. [x] Vue
+>    3. [ ] Ember
+`
+)
+
+const fullcontent = ref(
   `
   ## Color Syntax Plugin
 
@@ -73,4 +153,8 @@ $$
   `
 )
 
+const editorOptions = ref({
+  language:'en-US',
+  plugins:plugins
+}) 
 </script>
